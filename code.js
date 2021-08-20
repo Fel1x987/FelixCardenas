d6ea05e716356e7b43e59494ed071c110a550f0b
Fel1x987 <fcm68247@udelasalle.edu.mx>
2021-08-19 21:55:31 -0500

    Ya quedo

--- code.js
+++ code.js
@@ -1,7 +1,8 @@
 var t1 = document.getElementById("texto1");
 var t2 = document.getElementById("texto2");
 var cifrado1 = () =>{
-    var c = t1.value.split("");
+    let u = t1.value.toLowerCase();
+    var c = u.split("");
     var length = c.length;
     console.log(length);
     var m = "";
@@ -18,9 +19,84 @@ var cifrado1 = () =>{
         m += String.fromCharCode(cipher);
         cipher = 0;
     }
-
     t2.value = m;
 }
 
-//String.fromCharCode(65,66,67)
-//"ABC".charCodeAt(0) // returns 65
+var cifrado2 = () =>{
+    let u = t1.value.toLowerCase();
+    var c = u.split("");
+    var length = c.length;
+    console.log(length);
+    var m = "";
+    var cipher = 0;
+    for (var i = 0; i < length; i++) {
+        console.log(c[i].charCodeAt(0));
+        cipher = c[i].charCodeAt(0);
+        cipher += 3;
+        if(cipher == 123){
+            cipher = 97;
+        } else if (cipher == 124) {
+            cipher = 98;
+        } else if(cipher == 125){
+            cipher = 99;
+        }
+        m += String.fromCharCode(cipher);
+        cipher = 0;
+    }
+    t2.value = m;
+}
+
+var decode1 = () =>{
+    var c = t1.value.split("");
+    var length = c.length;
+    console.log(length);
+    var m = "";
+    var cipher = 0;
+    for (var i = 0; i < length; i++) {
+        console.log(c[i].charCodeAt(0));
+        cipher = c[i].charCodeAt(0);
+        switch (cipher) {
+            case 97:
+                cipher = 121;
+                break;
+            case 98: 
+                cipher = 122;
+            default:
+                cipher -= 2;
+        }
+        m += String.fromCharCode(cipher);
+        cipher = 0;
+    }
+    t2.value = m;
+}
+
+var decode2 = () =>{
+    var c = t1.value.split("");
+    var length = c.length;
+    console.log(length);
+    var m = "";
+    var cipher = 0;
+    for (var i = 0; i < length; i++) {
+        console.log(c[i].charCodeAt(0));
+        cipher = c[i].charCodeAt(0);
+        switch (cipher) {
+            case 97:
+                cipher = 120;
+                break;
+            case 98: 
+                cipher = 121;
+                break;
+            case 99:
+                cipher = 122;
+                break;
+            default:
+                cipher -= 3;
+        }
+        m += String.fromCharCode(cipher);
+        cipher = 0;
+    }
+    t2.value = m;
+}
+//abcdefghijklmnopqrstuvwxyz
+
+//cdefghijklmnopqrstuvwxyzab
